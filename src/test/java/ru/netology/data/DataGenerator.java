c833598fb293b5646694343a7d837be249d1058b
Andrey Shchepkin <lovenfed@gmail.com>
2021-09-23 13:05:17 +0300

    added allure

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,42 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationInfo generate(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new RegistrationInfo(
+                    faker.address().city(),
+                    faker.name().lastName() + " " + faker.name().firstName(),
+                    faker.phoneNumber().phoneNumber());
+        }
+
+        public static String generateDate(int days){
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+        public static String citiesValid(){
+            Random random = new Random();
+            int rand = random.nextInt(7);
+            String cities[] = {"Москва","Санкт-Петербург","Омск","Красноярск","Новосибирск","Казань","Уфа",};
+            return cities[rand];
+        }
+        public static String citiesNotValid(){
+            Random random = new Random();
+            int rand = random.nextInt(7);
+            String citiesNotVal[] = {"Сочи","Енисейск","Выборг","Приморск","Канск","Геленджик","Пушкин"};
+            return citiesNotVal[rand];
+        }
+    }
+}
---
+++ src/test/java/ru/netology/test/CardDeliveryTest.java
@@ -0,0 +1,107 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
+import ru.netology.data.RegistrationInfo;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataGenerator.Registration.citiesValid;
+import static ru.netology.data.DataGenerator.Registration.generateDate;
+
+public class CardDeliveryTest {
+    RegistrationInfo info = DataGenerator.Registration.generate("ru");
+
+@BeforeAll
+void setUpAll(){
+    SelenideLogger.addListener("allure", new AllureSelenide());
+}
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSuccessTest() {
+        $("[data-test-id='city'] input").setValue(citiesValid());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhoneNumber());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $(withText("Успешно!")).shouldBe(Condition.appear);
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(4)));
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(generateDate(6));
+        $(withText("Запланировать")).click();
+        $("[data-test-id='replan-notification'] .notification__title").shouldBe(Condition.appear);
+        $(withText("Перепланировать")).click();
+        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(6)));
+
+
+    }
+    @Test
+    void emptyCityField() {
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(generateDate(4));
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhoneNumber());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id='city'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+    @Test
+    void emptyNameField() {
+        $("[data-test-id='city'] input").setValue(citiesValid());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(generateDate(4));
+        $("[data-test-id='phone'] input").setValue(info.getPhoneNumber());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+    @Test
+    void emptyPhoneField() {
+        $("[data-test-id='city'] input").setValue(citiesValid());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").sendKeys(generateDate(4));
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+    @Test
+    void emptyDateField() {
+        $("[data-test-id='city'] input").setValue(citiesValid());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhoneNumber());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id='date'] .input_invalid .input__sub").shouldHave(Condition.text("Неверно введена дата"));
+    }
+    @Test
+    void citiesNotValid(){
+        $("[data-test-id='city'] input").setValue(DataGenerator.Registration.citiesNotValid());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(generateDate(4));
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhoneNumber());
+        $("[class=checkbox__box]").click();
+        $("[class=button__text]").click();
+        $("[data-test-id='city'] .input__sub").shouldHave(Condition.text("Доставка в выбранный город недоступна"));
+
+    }
+}
+
+
